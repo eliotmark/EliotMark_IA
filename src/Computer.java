5ed6d124f71ffc5813f687b9aadb7919d5f747a6
eliotmark <eliotmark@students.berkeley.net>
2020-04-02 13:56:02 -0700

    IA Project

--- src/Computer.java
+++ src/Computer.java
@@ -13,7 +13,7 @@ public class Computer extends Player {
 //        return col;
 //    }
 
-    // FIX THISSSS
+    // FIX THIS
     // Decides where the computer goes.
 //    public int determineMove(Board board) {
 //        int column = -1;
@@ -38,27 +38,75 @@ public class Computer extends Player {
 //        }
 //        return column;
 //    }
+
+    // Are duplicates bad? They show up as an error in the code but seem okay?
     public int determineMove(int playerMove) {
         int colNum = -1;
-        int rowNum = -1;
+
+        // Win if there are three in a row.
         for(int i=0; i<8; i++) {
-            colNum = threeInARowVertical(i);
-            // Do I need a different parameter?
-            colNum = twoInARowVertical(i);
-            rowNum = threeInARowHorizontal(i);
-            rowNum = twoInARowHorizontal(i);
-            if(colNum>-1) {
-                break;
-            }
-            if(rowNum>-1) {
+            colNum = threeInARowVerticalWin(i);
+            if(colNum != -1) {
                 break;
             }
         }
 
-        //return the column
+        for(int j=0; j<8; j++) {
+            colNum = threeInARowHorizontalWin(j);
+            if(colNum != -1) {
+                break;
+            }
+        }
+
+        // Block if there are three human tokens in a row.
+        for(int k=0; k<8; k++) {
+            colNum = threeInARowVerticalBlock(k);
+            if (colNum != -1) {
+                break;
+            }
+        }
+
+        for(int l=0; l<8; l++) {
+            colNum = threeInARowHorizontalBlock(l);
+            if (colNum != -1) {
+                break;
+            }
+        }
+
+        // Put down token if there are two in a row.
+        for(int m=0; m<8; m++) {
+            colNum = twoInARowVerticalWin(m);
+            if (colNum != -1) {
+                break;
+            }
+        }
+
+        for(int n=0; n<8; n++) {
+            colNum = twoInARowHorizontalWin(n);
+            if (colNum != -1) {
+                break;
+            }
+        }
+
+        // Block if there are two human tokens in a row.
+        for(int o=0; o<8; o++) {
+            colNum = twoInARowVerticalBlock(i);
+            if (colNum != -1) {
+                break;
+            }
+        }
+
+        for(int p=0; p<8; p++){
+            colNum = twoInARowHorizontalBlock(p);
+            if (colNum != -1) {
+                break;
+            }
+        }
+        return colNum;
     }
 
-    public int threeInARowVertical(int colNum) {
+    // Methods to block human.
+    public int threeInARowVerticalBlock(int colNum) {
         for(int row=0; row<8; row++) {
             if(board.getGrid()[row][colNum].equals("x")) {
                 if(row+1>6) {
@@ -82,8 +130,7 @@ public class Computer extends Player {
         return -1;
     }
 
-    //New parameter different than colNum?
-    public int twoInARowVertical(int colNum) {
+    public int twoInARowVerticalBlock(int colNum) {
         for(int row=0; row<8; row++) {
             if(board.getGrid()[row][colNum].equals("x")) {
                 if(row+1>6) {
@@ -92,7 +139,7 @@ public class Computer extends Player {
                     if(row+2>7) {
                         return -1;
                     }else{
-                        return -1;
+                        return colNum;
                     }
                 }else{
                     return -1;
@@ -105,7 +152,7 @@ public class Computer extends Player {
         return -1;
     }
 
-    public int threeInARowHorizontal(int rowNum) {
+    public int threeInARowHorizontalBlock(int rowNum) {
         for(int col=0; col<8; col++) {
             if(board.getGrid()[rowNum][col].equals("x")) {
                 if(col+1>6) {
@@ -114,7 +161,15 @@ public class Computer extends Player {
                     if(col+2>7) {
                         return -1;
                     }else if(board.getGrid()[rowNum][col+2].equals("x")) {
-                        return rowNum;
+                        if(col+3<7) {
+                            if(board.getGrid()[rowNum][col+3].equals("-") && !board.getGrid()[rowNum][col+3].equals("-")) {
+                                return col+3;
+                            }
+                        }else if(col-1>0) {
+                            if(board.getGrid()[rowNum][col-1].equals("-") && !board.getGrid()[rowNum][col-1].equals("-")) {
+                                return col-1;
+                            }
+                        }
                     }else{
                         return -1;
                     }
@@ -129,15 +184,45 @@ public class Computer extends Player {
         return -1;
     }
 
-    //Different parameter here too?
-    public int twoInARowHorizontal(int rowNum) {
+    public int twoInARowHorizontalBlock(int rowNum) {
         for(int col=0; col<8; col++) {
-            if(board.getGrid()[rowNum][col].equals("x")) {
-                if(col+1>6) {
+            if (board.getGrid()[rowNum][col].equals("x")) {
+                if (col + 1 > 6) {
                     return -1;
-                }else if(board.getGrid()[rowNum][col+1].equals("x")) {
-                    if(col+2>7) {
+                } else if (board.getGrid()[rowNum][col + 1].equals("x")) {
+                    if (col + 2 > 7) {
                         return -1;
+                    } else if (col + 2 < 7) {
+                        if (board.getGrid()[rowNum][col + 2].equals("-") && !board.getGrid()[rowNum][col + 2].equals("-")) {
+                            return col + 3;
+                        }
+                    } else if (col - 1 > 0) {
+                        if (board.getGrid()[rowNum][col - 1].equals("-") && !board.getGrid()[rowNum][col - 1].equals("-")) {
+                            return col - 1;
+                        }
+                    }
+                }
+            } else {
+                return -1;
+            }
+            if (board.getGrid()[rowNum][col].equals("o")) {
+                return -1;
+            }
+        }
+        return -1;
+    }
+
+    // Methods to win against human.
+    public int threeInARowVerticalWin(int colNum) {
+        for(int row=0; row<8; row++) {
+            if(board.getGrid()[row][colNum].equals("o")) {
+                if(row+1>6) {
+                    return -1;
+                }else if(board.getGrid()[row+1][colNum].equals("o")) {
+                    if(row+2>7) {
+                        return -1;
+                    }else if(board.getGrid()[row+2][colNum].equals("o")) {
+                        return colNum;
                     }else{
                         return -1;
                     }
@@ -145,7 +230,90 @@ public class Computer extends Player {
                     return -1;
                 }
             }
+            if(board.getGrid()[row][colNum].equals("x")) {
+                return -1;
+            }
+        }
+        return -1;
+    }
+
+    public int twoInARowVerticalWin(int colNum) {
+        for(int row=0; row<8; row++) {
+            if(board.getGrid()[row][colNum].equals("o")) {
+                if(row+1>6) {
+                    return -1;
+                }else if(board.getGrid()[row+1][colNum].equals("o")) {
+                    if(row+2>7) {
+                        return -1;
+                    }else{
+                        return colNum;
+                    }
+                }else{
+                    return -1;
+                }
+            }
+            if(board.getGrid()[row][colNum].equals("x")) {
+                return -1;
+            }
+        }
+        return -1;
+    }
+
+    //There are duplicates.. is this bad?
+    public int threeInARowHorizontalWin(int rowNum) {
+        for(int col=0; col<8; col++) {
             if(board.getGrid()[rowNum][col].equals("o")) {
+                if(col+1>6) {
+                    return -1;
+                }else if(board.getGrid()[rowNum][col+1].equals("o")) {
+                    if(col+2>7) {
+                        return -1;
+                    }else if(board.getGrid()[rowNum][col+2].equals("o")) {
+                        if(col+3<7) {
+                            if(board.getGrid()[rowNum][col+3].equals("-") && !board.getGrid()[rowNum][col+3].equals("-")) {
+                                return col+3;
+                            }
+                        }else if(col-1>0) {
+                            if(board.getGrid()[rowNum][col-1].equals("-") && !board.getGrid()[rowNum][col-1].equals("-")) {
+                                return col-1;
+                            }
+                        }
+                    }else{
+                        return -1;
+                    }
+                }else{
+                    return -1;
+                }
+            }
+            if(board.getGrid()[rowNum][col].equals("x")) {
+                return -1;
+            }
+        }
+        return -1;
+    }
+
+    public int twoInARowHorizontalWin(int rowNum) {
+        for(int col=0; col<8; col++) {
+            if (board.getGrid()[rowNum][col].equals("o")) {
+                if (col + 1 > 6) {
+                    return -1;
+                } else if (board.getGrid()[rowNum][col + 1].equals("o")) {
+                    if (col + 2 > 7) {
+                        return -1;
+                    } else if (col + 2 < 7) {
+                        if (board.getGrid()[rowNum][col + 2].equals("-") && !board.getGrid()[rowNum][col + 2].equals("-")) {
+                            return col + 3;
+                        }
+                    } else if (col - 1 > 0) {
+                        if (board.getGrid()[rowNum][col - 1].equals("-") && !board.getGrid()[rowNum][col - 1].equals("-")) {
+                            return col - 1;
+                        }
+                    }
+                }
+            } else {
+                return -1;
+            }
+            if (board.getGrid()[rowNum][col].equals("x")) {
                 return -1;
             }
         }
